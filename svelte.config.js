fcef957c35ad1abc3cf9cbdb3233bdec84185bad
Edvin Loberg <edvin.loberg@gmail.com>
2022-11-03 20:13:03 +0100

    vercel and postcss stuff

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
 import vercel from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
+import autoprefixer from "autoprefixer";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		postcss: {
+			plugins: [autoprefixer]
+		}
+	}),
 
 	kit: {
 		adapter: vercel()
