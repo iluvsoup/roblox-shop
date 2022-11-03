ad1d7bea82d6314971c0700b7c026cc0e6b1a9e7
Edvin Loberg <edvin.loberg@gmail.com>
2022-11-03 23:13:27 +0100

    fix env stuff

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
-import { REDIS_URL } from "$env/static/private";
-
-const _redis = new Redis(REDIS_URL);
+console.log(process.env);
+// import { REDIS_URL } from "$env/static/private";
+const _redis = new Redis(process.env.REDIS_URL!);
 _redis.disconnect();
 
 export const redis = _redis;
