b5709f8d2f6d15041547195e5a56175de50eff67
Edvin Loberg <edvin.loberg@gmail.com>
2022-11-04 12:42:25 +0100

    update dependencies

--- src/lib/components/Spinner.svelte
+++ src/lib/components/Spinner.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	// import Icon from "svelte-awesome/components/Icon.svelte";
-	// import gear from "svelte-awesome/icons/gear";
+	// import {}
 </script>
 
 <div class="gear">
-	<!-- <Icon data={gear} scale={5} spin /> -->
 </div>
