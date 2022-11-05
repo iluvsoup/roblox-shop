b96c3f67c36b98c78e9bf60c451fd9381ee9d77c
Edvin Loberg <edvin.loberg@gmail.com>
2022-11-05 10:29:21 +0100

    add a missing depenency and remove some unused ones

--- src/lib/particles.ts
+++ src/lib/particles.ts
@@ -2,6 +2,7 @@ import { loadBaseMover } from "tsparticles-move-base";
 import { loadCircleShape } from "tsparticles-shape-circle";
 import { loadColorUpdater } from "tsparticles-updater-color";
 import { loadOpacityUpdater } from "tsparticles-updater-opacity";
+import { loadOutModesUpdater } from "tsparticles-updater-out-modes";
 
 import type { Engine } from "tsparticles-engine";
 
@@ -10,4 +11,5 @@ export const loadParticles = async (engine: Engine) => {
 	await loadCircleShape(engine);
 	await loadOpacityUpdater(engine);
 	await loadColorUpdater(engine);
+	await loadOutModesUpdater(engine);
 };
