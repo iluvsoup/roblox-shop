8f99123f42bf54fc07b47a27e152bbae3f1a3c70
Edvin Loberg <edvin.loberg@gmail.com>
2022-11-10 20:56:52 +0100

    fix url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 // duration in seconds
 export const TOKEN_DURATION = 60 * 60 * 24 * 30;
-export const URL = "http://localhost:5173";
+export const URL =
+	process.env.NODE_ENV == "development" ? "http://localhost:5173" : "https://shop.iluvsoup.com";
