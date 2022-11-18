16318704241348c57bb81bee172f3ab1c7f27ad3
Edvin Loberg <edvin.loberg@gmail.com>
2022-11-18 20:17:43 +0100

    Update product page

--- src/routes/shop/products/[product]/+page.svelte
+++ src/routes/shop/products/[product]/+page.svelte
@@ -33,35 +33,146 @@
 </svelte:head>
 
 <template>
-	<img class="productimage" src={data.productdata.imageurl} alt="product" />
-	<h1>{data.productdata.name}</h1>
+	<div class="card">
+		<img class="image" src={data.productdata.imageurl} alt="product" />
 
-	{#if priceObject.type == "recurring"}
-		<h1 class="green pricetext">
-			{price} every
-			{#if priceObject.recurring?.interval_count == 1}
-				{priceObject.recurring?.interval}
+		<div class="content">
+			<h1 class="name">{data.productdata.name}</h1>
+			<div class="divider" />
+
+			<div class="price">
+				{#if price.symbolOnLeft}
+					<p class="pricesymbol left">{price.symbol}</p>
+					<p class="pricetext">{price.number}</p>
+				{:else}
+					<p class="pricetext">{price.number}</p>
+					<p class="pricesymbol right">{price.symbol}</p>
+				{/if}
+			</div>
+
+			<p class="description">{data.productdata.description}</p>
+
+			{#if redirecting == true}
+				<button class="buy">Redirecting...</button>
 			{:else}
-				{priceObject.recurring?.interval_count} {priceObject.recurring?.interval}s
+				<button class="buy" on:click={checkout}>BUY NOW!!!</button>
 			{/if}
-		</h1>
-	{:else}
-		<h1 class="green pricetext">{price}</h1>
-	{/if}
-
-	{#if redirecting == true}
-		<button>Redirecting...</button>
-	{:else}
-		<button on:click={checkout}>BUY NOW!!!</button>
-	{/if}
+		</div>
+	</div>
 </template>
 
 <style>
-	.productimage {
-		width: 15rem;
+	.card {
+		background-color: var(--primary-light);
+		border-radius: 0.5rem;
+		overflow: hidden;
+		width: 100%;
+		max-width: 500px;
+		box-shadow: 0 0.125rem 0.5rem 0.25rem rgba(0, 0, 0, 0.25);
+		margin-right: auto;
+		margin-left: auto;
+		color: #fff;
 	}
 
-	.green {
-		color: green;
+	.image {
+		width: 100%;
+	}
+
+	.content {
+		padding-left: 3rem;
+		padding-right: 3rem;
+		padding-bottom: 1.5rem;
+		padding-top: 1rem;
+	}
+
+	.name {
+		text-align: center;
+		font-size: 38px;
+		margin-top: 0;
+		margin-bottom: 1rem;
+	}
+
+	.divider {
+		width: 100%;
+		height: 2px;
+		background-color: #333;
+		border-radius: 2px;
+	}
+
+	.price {
+		display: flex;
+		justify-content: center;
+		align-items: baseline;
+		margin-top: 0.5rem;
+		margin-bottom: 0;
+	}
+
+	.pricesymbol {
+		/* user-select: none; */
+		display: inline;
+		margin: 0;
+		padding: 0;
+		font-weight: 500;
+		font-size: 28px;
+	}
+
+	.pricesymbol.left {
+		margin-right: 2px;
+	}
+
+	.pricesymbol.right {
+		margin-left: 2px;
+	}
+
+	.pricetext {
+		display: inline;
+		margin: 0;
+		padding: 0;
+		text-align: center;
+		font-weight: 600;
+		font-size: 48px;
+		background-image: linear-gradient(var(--secondary-light), var(--secondary-bright));
+		-webkit-background-clip: text;
+		background-clip: text;
+		color: #0000;
+	}
+
+	.description {
+		text-align: center;
+		color: #888;
+		font-size: 18px;
+		margin-top: 0.5rem;
+		width: 25ch;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	.buy {
+		margin-left: auto;
+		margin-right: auto;
+		display: block;
+		background-color: var(--secondary);
+		border: 1px solid var(--secondary);
+		border-radius: 0.5rem;
+		color: #fff;
+		font-weight: 700;
+		font-size: 18px;
+		cursor: pointer;
+		transition: box-shadow, 0.5s ease;
+		font-family: Poppins;
+		padding-left: 1rem;
+		padding-right: 1rem;
+		padding-top: 0.5rem;
+		padding-bottom: 0.5rem;
+	}
+
+	.buy:hover,
+	.buy:focus {
+		outline: none;
+		/* transform: translateY(-0.125rem); */
+		box-shadow: 0 0.125rem 0.25rem 0 rgba(0, 0, 0, 0.25);
+		background-color: #fff;
+		border: 1px solid var(--secondary);
+		color: var(--secondary);
 	}
 </style>
