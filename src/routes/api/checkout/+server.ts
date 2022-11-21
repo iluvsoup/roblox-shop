ce02c86ff879af09e18adc1b7920b65554a11c0c
Edvin Loberg <edvin.loberg@gmail.com>
2022-11-21 11:46:04 +0100

    remove redunant null check

--- src/routes/api/checkout/+server.ts
+++ src/routes/api/checkout/+server.ts
@@ -27,23 +27,19 @@ export const POST: RequestHandler = async ({ request, cookies }) => {
 
 	const price = await stripe.prices.retrieve(priceId);
 
-	if (price) {
-		const session = await stripe.checkout.sessions.create({
-			customer: await getOrCreateCustomer(data.uid),
-			payment_method_types: price.currency === "brl" ? ["card", "pix"] : ["card"],
-			line_items: [
-				{
-					price: priceId,
-					quantity: 1 // hard coded for now
-				}
-			],
-			mode: "payment",
-			cancel_url: `${getUrl()}/shop`,
-			success_url: `${getUrl()}/shop/success/{CHECKOUT_SESSION_ID}`
-		});
+	const session = await stripe.checkout.sessions.create({
+		customer: await getOrCreateCustomer(data.uid),
+		payment_method_types: price.currency === "brl" ? ["card", "pix"] : ["card"],
+		line_items: [
+			{
+				price: priceId,
+				quantity: 1 // hard coded for now
+			}
+		],
+		mode: "payment",
+		cancel_url: `${getUrl()}/shop`,
+		success_url: `${getUrl()}/shop/success/{CHECKOUT_SESSION_ID}`
+	});
 
-		return new Response(session.url);
-	} else {
-		throw error(400, { message: "No product associated with request" });
-	}
+	return new Response(session.url);
 };
