8acde8e077b7857d0eff1b696957b9375559f6cd
Edvin Loberg <edvin.loberg@gmail.com>
2022-11-29 20:25:54 +0100

    add app.css

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
 // duration in seconds
 export const TOKEN_DURATION = 60 * 60 * 24 * 30;
-
 export const PIX_ENABLED = false;
 
+// Made a function for it because variable was funky
 export const getUrl = () => {
-	return process.env.NODE_ENV == "development" ? "http://192.168.1.156:5173" : "https://shop.iluvsoup.com";
+	return process.env.NODE_ENV == "development" ? "http://localhost:5173" : "https://shop.iluvsoup.com";
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,23 +24,6 @@
 </template>
 
 <style>
-	@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
-
-	:root {
-		--primary: rgb(27, 27, 27);
-		--primary-dark: rgb(15, 15, 15);
-		--primary-light: rgb(39, 39, 39);
-		--secondary: rgb(0, 85, 255);
-		--secondary-alternate: rgb(0, 47, 255);
-		--secondary-bright: rgb(103, 154, 255);
-		--secondary-light: rgb(67, 130, 255);
-		font-family: Poppins;
-	}
-
-	:global(body) {
-		margin: 0;
-	}
-
 	.navigating {
 		width: 100%;
 		height: 100%;
