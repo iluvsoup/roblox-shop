7b586f3ba56e73610c749b24327061385ce64986
iluvsoup <94307595+iluvsoup@users.noreply.github.com>
2022-11-30 21:40:56 +0100

    Update URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,5 +4,5 @@ export const PIX_ENABLED = false;
 
 // Made a function for it because variable was funky
 export const getUrl = () => {
-	return process.env.NODE_ENV == "development" ? "http://localhost:5173" : "https://shop.iluvsoup.com";
+	return process.env.NODE_ENV == "development" ? "http://localhost:5173" : "https://exercitobrasileiro.vercel.app";
 };
