0f241ab35d9e5b0142962a38ad194ca473860cca
iluvsoup <94307595+iluvsoup@users.noreply.github.com>
2022-12-02 00:13:43 +0100

    add check for if customer exists

--- src/lib/server/get_or_create_customer.ts
+++ src/lib/server/get_or_create_customer.ts
@@ -2,6 +2,21 @@ import { stripe } from "$lib/server/stripe";
 import { prisma } from "$lib/server/prisma";
 import { error } from "@sveltejs/kit";
 
+const createCustomer = async (uid: string) => {
+	const customer = await stripe.customers.create({
+		metadata: {
+			uid: uid
+		}
+	});
+
+	await prisma.user.update({
+		where: { uid: uid },
+		data: { stripeCustomerId: customer.id }
+	});
+
+	return customer.id;
+}
+
 export const getOrCreateCustomer = async (uid: string) => {
 	const user = await prisma.user.findUnique({
 		where: { uid: uid }
@@ -9,20 +24,16 @@ export const getOrCreateCustomer = async (uid: string) => {
 
 	if (user) {
 		if (user?.stripeCustomerId) {
-			return user.stripeCustomerId;
+			const customer = await stripe.customers.retrieve(user.stripecustomerId)
+			
+			if (customer) {
+				return user.stripeCustomerId;
+			} else {
+				// customer id is invalid
+				return createCustomer(uid)
+			}
 		} else {
-			const customer = await stripe.customers.create({
-				metadata: {
-					uid: uid
-				}
-			});
-
-			await prisma.user.update({
-				where: { uid: uid },
-				data: { stripeCustomerId: customer.id }
-			});
-
-			return customer.id;
+			return createCustomer(uid)
 		}
 	} else {
 		throw error(500, { message: "Could not find user, please log in again" });
